0fbb35630939f3057c5269bdc91c6c5966caeacf
SirGhaniR <ghanifaustaraihan@gmail.com>
2026-05-29 21:54:38 +0700

    feat(backend): use auth and admin middleware in movie routes

--- backend/src/routes/movieRoutes.js
+++ backend/src/routes/movieRoutes.js
@@ -6,13 +6,17 @@ import {
   listMovies,
   removeMovie,
 } from "../controllers/movieController.js";
+import {
+  adminMiddleware,
+  authMiddleware,
+} from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
 router.get("/", listMovies);
 router.get("/:id", getMovie);
-router.post("/", addMovie);
-router.patch("/:id", editMovie);
-router.delete("/:id", removeMovie);
+router.post("/", authMiddleware, adminMiddleware, addMovie);
+router.patch("/:id", authMiddleware, adminMiddleware, editMovie);
+router.delete("/:id", authMiddleware, adminMiddleware, removeMovie);
 
 export { router as movieRoutes };
