d1d3bea1b539b13a96a5ca8a88ee277d10d7f533
SirGhaniR <ghanifaustaraihan@gmail.com>
2026-05-31 21:41:05 +0700

    setup: add shared API client helper for backend requests

---
+++ frontend/src/api/client.js
@@ -0,0 +1,26 @@
+const API_BASE = "http://localhost:3000/api";
+
+export async function apiRequest(endpoint, options = {}) {
+  const token = localStorage.getItem("token");
+
+  const headers = {
+    "Content-Type": "application/json",
+    ...(token && { Authorization: `Bearer ${token}` }),
+    ...options.headers,
+  };
+
+  const res = await fetch(`${API_BASE}${endpoint}`, {
+    ...options,
+    headers,
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    const error = new Error(data.message || "Something went wrong");
+    error.status = res.status;
+    throw error;
+  }
+
+  return data;
+}
