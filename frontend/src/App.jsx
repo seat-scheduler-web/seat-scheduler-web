222ba91dd92dcc4d6462067f9708f6e8130b8e20
Raihan Ghani Fausta <ghanifaustaraihan@gmail.com>
2026-06-01 23:14:27 +0700

    feat: add sticky footer with project info and social links (#70)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider, useAuth } from "./context/AuthContext";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import { ToastProvider } from "./components/Toast";
 import PageTransition from "./components/PageTransition";
 import Home from "./pages/Home";
@@ -23,87 +24,90 @@ function AppRoutes() {
   }
 
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <PageTransition>
-              <Home />
-            </PageTransition>
-          }
-        />
-        <Route
-          path="/schedules/:id"
-          element={
-            <PageTransition>
-              <ScheduleDetail />
-            </PageTransition>
-          }
-        />
-        <Route
-          path="/schedules/:id/seats"
-          element={
-            user ? (
+      <main className="flex-1">
+        <Routes>
+          <Route
+            path="/"
+            element={
               <PageTransition>
-                <SeatSelection />
+                <Home />
               </PageTransition>
-            ) : (
-              <Navigate to="/login" />
-            )
-          }
-        />
-        <Route
-          path="/bookings/:id/confirmation"
-          element={
-            user ? (
+            }
+          />
+          <Route
+            path="/schedules/:id"
+            element={
               <PageTransition>
-                <BookingConfirmation />
+                <ScheduleDetail />
               </PageTransition>
-            ) : (
-              <Navigate to="/login" />
-            )
-          }
-        />
-        <Route
-          path="/my-bookings"
-          element={
-            user ? (
-              <PageTransition>
-                <MyBookings />
-              </PageTransition>
-            ) : (
-              <Navigate to="/login" />
-            )
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            user ? (
-              <Navigate to="/" />
-            ) : (
-              <PageTransition>
-                <Login />
-              </PageTransition>
-            )
-          }
-        />
-        <Route
-          path="/register"
-          element={
-            user ? (
-              <Navigate to="/" />
-            ) : (
-              <PageTransition>
-                <Register />
-              </PageTransition>
-            )
-          }
-        />
-      </Routes>
-    </>
+            }
+          />
+          <Route
+            path="/schedules/:id/seats"
+            element={
+              user ? (
+                <PageTransition>
+                  <SeatSelection />
+                </PageTransition>
+              ) : (
+                <Navigate to="/login" />
+              )
+            }
+          />
+          <Route
+            path="/bookings/:id/confirmation"
+            element={
+              user ? (
+                <PageTransition>
+                  <BookingConfirmation />
+                </PageTransition>
+              ) : (
+                <Navigate to="/login" />
+              )
+            }
+          />
+          <Route
+            path="/my-bookings"
+            element={
+              user ? (
+                <PageTransition>
+                  <MyBookings />
+                </PageTransition>
+              ) : (
+                <Navigate to="/login" />
+              )
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              user ? (
+                <Navigate to="/" />
+              ) : (
+                <PageTransition>
+                  <Login />
+                </PageTransition>
+              )
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              user ? (
+                <Navigate to="/" />
+              ) : (
+                <PageTransition>
+                  <Register />
+                </PageTransition>
+              )
+            }
+          />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
